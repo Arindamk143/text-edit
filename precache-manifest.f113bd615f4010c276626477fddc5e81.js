e49fddf45e07a850b0e94c4a357a310f1ae092a7
Arindamk143 <102083962+Arindamk143@users.noreply.github.com>
2022-07-12 14:07:10 +0530

    Updates

---
+++ precache-manifest.f113bd615f4010c276626477fddc5e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c52919646a341db52d2",
+    "url": "/text-edit/static/css/main.af81acc4.chunk.css"
+  },
+  {
+    "revision": "8c52919646a341db52d2",
+    "url": "/text-edit/static/js/main.8c529196.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/text-edit/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "48878bb103e3917c1593",
+    "url": "/text-edit/static/js/2.48878bb1.chunk.js"
+  },
+  {
+    "revision": "0bef2baeed0b440ecccf",
+    "url": "/text-edit/static/js/runtime~main.0bef2bae.js"
+  },
+  {
+    "revision": "2bbbf9cd082f5540f2f32d3345e61daf",
+    "url": "/text-edit/index.html"
+  }
+];
